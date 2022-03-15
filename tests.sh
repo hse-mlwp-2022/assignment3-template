35814f3317d780b4c43a34da566b53ff9a269385
Mark Polyak <markpolyak@gmail.com>
2022-03-15 13:19:46 +0300

    Run pytest from GitHub Classroom

--- tests.sh
+++ tests.sh
@@ -8,4 +8,4 @@ pip3 install pytest pytest-cov pytest-github-actions-annotate-failures jupyter n
 wget -N https://raw.githubusercontent.com/hse-mlwp-2022/assignment3-template/main/tests.py
 jupyter nbconvert --to script --output "pandas_assignment_dirty" pandas_exercise.ipynb
 cat pandas_assignment_dirty.py | grep -v "^#" | grep -v "^$" | grep -v "get_ipython()" | grep -vE "(\s|=|^)input\(" > pandas_exercise.py
-pytest -s tests.py
+# pytest -s tests.py
